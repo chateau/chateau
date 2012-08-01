db5a7f711af206dd4887c7b5e3b98b0ffa8dfc7e
Kudu <kudu.py@gmail.com>
2012-07-31 20:42:33 -0400

    more work on plugins

--- chateau/plugin.py
+++ chateau/plugin.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 
 class Plugin(object):
+    """A plugin. This is meant to be subclassed."""
+    
+    def __init__(self, bot):
+        self.apply_method_subscriptions()
+    
+    def apply_method_subscriptions(self):
+        """Apply subscriptions "reserved" using :py:func:`.subscribe`. Called automatically by :py:func:`__init__`."""
+    
+    @classmethod
     def subscribe(fn, *types):
-        new = set(types)
+        """Flags an unbound instance method for subscription to certain event types by :py:func:`.apply_method_subscriptions`. Intended for use as a decorator.
+        
+        Example::
+           class SomePlugin(Plugin):
+               @subscribe('NICK')
+               def on_nick_change:
+                   pass
+        
+        """
+        new = set(types) # The new event types to subscribe to
         try:
-            old = getattr(fn, 'subscription')
-            union = old | new
+            old = getattr(fn, 'subscribed_to') # Get any event types to which the function is already subscribed
+            union = old | new # Merge the two sets. Equivalent to old.union(new).
         except AttributeError:
-            union = new
-        setattr(fn, 'subscription', union)
+            union = new # If there is no existing subscription, just save the new ones.
+        setattr(fn, 'subscribed_to', union) # Save the types the function is subscribed to
+        return fn # Same object
